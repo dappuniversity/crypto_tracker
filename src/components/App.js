943bc06f9284dfaeac5126631ac4e507b75f8745
gwmccubbin <gwmccubbin@gmail.com>
2020-10-12 12:09:15 -0500

    Adds better starter code

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,68 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
-import './App.css';
+import chart from '../logos/chart.png';
+import btc from '../logos/btc.png';
+import eth from '../logos/eth.png';
+import link from '../logos/link.png';
+import ada from '../logos/ada.png';
+import xmr from '../logos/xmr.png';
+import yfi from '../logos/yfi.png';
+import lend from '../logos/lend.png';
+import comp from '../logos/comp.png';
+import uni from '../logos/uni.png';
+import gnt from '../logos/gnt.png';
+
+const axios = require("axios");
 
 class App extends Component {
+
+  // TODO: LOAD DATA FROM API HERE...
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      loading: true
+    }
+  }
+
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow text-monospace text-white">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
             href="http://www.dappuniversity.com/bootcamp"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Dapp University
+            <img src={chart} width="30" height="30" className="d-inline-block align-top" alt="" />
+            Crypt0 Track3r
           </a>
+          {/* TODO: Code goes here... */}
         </nav>
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
-              </div>
-            </main>
+        &nbsp;
+          <div className="container-fluid mt-5 w-50 p-3">
+            <div className="row">
+              <main role="main" className="col-lg-12 d-flex text-center">
+                  <table className="table table-striped table-hover table-fixed table-bordered text-monospace">
+                    <caption>Data Source:
+                      <a target="_blank" rel="noopener noreferrer" href="https://coinpaprika.com/">coinpaprika</a>
+                    </caption>
+                    <thead className="thead-dark">
+                      <tr>
+                        <th scope="col">Rank</th>
+                        <th scope="col">Logo</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Market Cap</th>
+                      </tr>
+                    </thead>
+                      <tbody>
+                        {/* TODO: Code goes here... */}
+                      </tbody>
+                  </table>
+              </main>
+            </div>
           </div>
-        </div>
       </div>
     );
   }
